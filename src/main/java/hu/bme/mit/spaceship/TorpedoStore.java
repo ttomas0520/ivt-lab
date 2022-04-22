b5c281c6feb287126c96652f5d6cfe2f52dc8528
ttomas0520 <ttomas2005@gmail.com>
2022-04-22 11:04:48 +0200

    Random generator one object fix

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -32,6 +32,7 @@ public class TorpedoStore {
     }
   }
 
+  private Random generator=new Random();
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
@@ -40,7 +41,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
+  
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
